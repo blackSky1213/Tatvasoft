16ab02cf269ddbf12652567b84e9aeff393875c3
kripc <42443511+blackSky1213@users.noreply.github.com>
2022-01-25 18:35:27 +0530

    connect static page with backend

---
+++ helperland/HelperLand/HelperLand/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using HelperLand.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelperLand.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return PartialView();
+        }
+
+        public IActionResult About()
+        {
+            return PartialView();
+        }
+
+        public IActionResult Price()
+        {
+            return PartialView();
+        }
+
+        public IActionResult Faq()
+        {
+            return PartialView();
+        }
+
+        public IActionResult Contact()
+        {
+            return PartialView();
+        }
+
+        public IActionResult BecomePro()
+        {
+            return PartialView();
+        }
+
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
