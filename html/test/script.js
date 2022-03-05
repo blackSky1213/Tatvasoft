3508bc0e031d645952f4400cebd883fa2b083726
Kirpalsinh Sarvaiya <kbsarvaiya123@gmail.com>
2022-03-05 08:20:18 +0530

    create rating dashboard, upcoming service dasboard

---
+++ html/test/script.js
@@ -0,0 +1,7 @@
+var avr = Math.round(3.05, 1);
+
+var colorStart = document.getElementsByClassName("half-star");
+
+for (var i = 0; i < 3.5 * 2; i++) {
+  colorStart[i].classList.add("half-start");
+}
